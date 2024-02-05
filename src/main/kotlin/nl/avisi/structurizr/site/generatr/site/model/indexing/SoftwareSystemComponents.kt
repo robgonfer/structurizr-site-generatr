28ba71e4d4e5e4ffd1ead3c8f4c87f4eca6be787
robgonfer <robgonfer@gmail.com>
2024-02-05 17:21:39 +0100

    Update SoftwareSystemComponents.kt

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/indexing/SoftwareSystemComponents.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/indexing/SoftwareSystemComponents.kt
@@ -1,6 +1,7 @@
 package nl.avisi.structurizr.site.generatr.site.model.indexing
 
 import com.structurizr.model.SoftwareSystem
+import nl.avisi.structurizr.site.generatr.site.GeneratorContext
 import nl.avisi.structurizr.site.generatr.site.asUrlToDirectory
 import nl.avisi.structurizr.site.generatr.site.model.PageViewModel
 import nl.avisi.structurizr.site.generatr.site.model.SoftwareSystemPageViewModel
