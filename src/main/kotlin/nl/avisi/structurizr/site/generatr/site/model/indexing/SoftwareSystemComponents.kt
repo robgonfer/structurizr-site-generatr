35ec997c88f879c2a4a816d308bd93455602abd7
robgonfer <robgonfer@gmail.com>
2024-06-11 15:24:17 +0200

    Improve Search (#8)
    * Update SoftwareSystemComponents.kt
    
    * Update SoftwareSystemComponents.kt
    
    * Update SoftwareSystemComponents.kt
    
    * Update SoftwareSystemComponents.kt
    
    * Update SoftwareSystemComponents.kt
    
    * Update SoftwareSystemComponents.kt
    
    * Update SoftwareSystemComponents.kt
    
    * Update SoftwareSystemComponents.kt
    
    * Update SoftwareSystemComponents.kt

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/indexing/SoftwareSystemComponents.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/indexing/SoftwareSystemComponents.kt
@@ -9,30 +9,32 @@ import nl.avisi.structurizr.site.generatr.site.model.PageViewModel
 import nl.avisi.structurizr.site.generatr.site.model.SoftwareSystemPageViewModel
 
 fun softwareSystemComponents(softwareSystem: SoftwareSystem, viewModel: PageViewModel) = softwareSystem.containers
-    .sortedBy { it.name }
-    .flatMap { container ->
-        container.components
-            .sortedBy { it.name }
-            .flatMap { component ->
-                listOf(component.name, component.description, component.technology)
-            }
-    }
-    .filter { it != null && it.isNotBlank() }
-    .joinToString(" ")
-    .ifBlank { null }
-    ?.let {
-        Document(
-            SoftwareSystemPageViewModel.url(softwareSystem, SoftwareSystemPageViewModel.Tab.COMPONENT)
-                .asUrlToDirectory(viewModel.url),
-            "Component views",
-            "${softwareSystem.name} | Component views",
-            it
-        )
-    }
+        .sortedBy { it.name }
+        .flatMap { container ->
+            container.components
+                    .sortedBy { it.name }
+                    .flatMap { component ->
+                        listOf(component.name, component.description, component.technology)
+                    }
+        }
+        .filter { it != null && it.isNotBlank() }
+        .joinToString(" ")
+        .ifBlank { null }
+        ?.let {
+            Document(
+                    SoftwareSystemPageViewModel.url(softwareSystem, SoftwareSystemPageViewModel.Tab.COMPONENT)
+                            .asUrlToDirectory(viewModel.url),
+                    "Component views",
+                    "${softwareSystem.name} | Component views",
+                    it
+            )
+        }
 
-    fun softwareSystemComponentsComponent(softwareSystem: SoftwareSystem, viewModel: PageViewModel, generatorContext: GeneratorContext) : List<Document> {
+fun softwareSystemComponentsComponent(softwareSystem: SoftwareSystem, viewModel: PageViewModel, generatorContext: GeneratorContext) : List<Document> {
 
-    val allSoftwareSystems = generatorContext.workspace.includedSoftwareSystems
+    val allSoftwareSystems = softwareSystem.model.softwareSystems        
+    val allExternalSoftwareSystems = allSoftwareSystems.filter { x -> x.hasTag("FakeExternalSystem") ||  x.hasTag("External System")}
+    
     val allComponents = mutableListOf<Component>()
     allSoftwareSystems.forEach {ss ->
 
@@ -41,7 +43,7 @@ fun softwareSystemComponents(softwareSystem: SoftwareSystem, viewModel: PageView
                 .toList())
 
     }
-        
+
     var components = softwareSystem.containers
             .sortedBy { it.name }
             .flatMap { container -> container.components }
@@ -54,21 +56,21 @@ fun softwareSystemComponents(softwareSystem: SoftwareSystem, viewModel: PageView
 
     val allDigs = generatorContext.workspace.views.componentViews
             .sortedBy { it.key }
-            
+
 
     components.forEach {
 
         val dig = diagrams.firstOrNull { v -> v.container.id == it.container.id }
-        
+
         val href = SoftwareSystemPageViewModel.url(softwareSystem, SoftwareSystemPageViewModel.Tab.COMPONENT)
-                .asUrlToDirectory(viewModel.url)        
+                .asUrlToDirectory(viewModel.url)
         documents += Document(
                 GetUrl(dig?.key ?: "NOTFOUND", href),
                 "Component views",
                 "${softwareSystem.name} | Component views | ${it.container.name} | ${it.name}",
                 it.name)
 
-        //Add linked components
+        //Add linked components and External SS
         if (dig != null)
         {
             //Need to find all components with a relationship with IT as destination
@@ -78,26 +80,86 @@ fun softwareSystemComponents(softwareSystem: SoftwareSystem, viewModel: PageView
                 c.relationships
                         .filter { r -> r.destination.id.equals(it.id) }
                         .forEach {rl ->
-                                componentsWithRel.add(c)
+                            componentsWithRel.add(c)
                         }
             }
 
             componentsWithRel.forEach { linkedct ->
 
-                    val relDig = allDigs.firstOrNull { v -> v.container.id == linkedct.container.id }
+                val relDig = allDigs.firstOrNull { v -> v.container.id == linkedct.container.id }
 
-                    if (relDig?.key?.isNotEmpty() == true)
-                    {
+                if (relDig?.key?.isNotEmpty() == true)
+                {
 
-                        documents += Document(
-                                GetUrl(relDig.key, href),
-                                "Component views",
-                                "${softwareSystem.name} | Component views | ${linkedct.container.name} | ${linkedct.name} | (INBOUND)",
-                                it.name)
-                    }
+                    documents += Document(
+                            GetUrl(relDig.key, href),
+                            "Component views",
+                            "${softwareSystem.name} | Component views | ${linkedct.container.name} | ${linkedct.name} | (INBOUND)",
+                            it.name)
+                }
 
             };
-        }        
+
+            //Now all external software systems. Get Inbound
+            val externalSSWithRel = mutableListOf<SoftwareSystem>()
+
+            allExternalSoftwareSystems.forEach { s ->
+                s.relationships
+                        .filter { r -> r.destination.id.equals(it.id) }
+                        .forEach {rl ->
+                            externalSSWithRel.add(s)
+                        }
+            }
+
+            externalSSWithRel.forEach { linkedExternalSS ->
+
+                val relDig = diagrams.firstOrNull { v -> v.container.id == it.container.id }
+
+                if (relDig?.key?.isNotEmpty() == true)
+                {
+
+                    documents += Document(
+                            GetUrl(relDig.key, href),
+                            "Component views",
+                            "${softwareSystem.name} | Component views | ${it.container.name} | ${it.name} | ${linkedExternalSS.name} | (INBOUND)",
+                            linkedExternalSS.name)
+
+                    documents += Document(
+                            GetUrl(relDig.key, href),
+                            "Component views",
+                            "${softwareSystem.name} | Component views | ${it.container.name} | ${it.name} | ${linkedExternalSS.name} | (INBOUND)",
+                            it.name)                    
+                }
+
+            };
+                
+            //Now outbound calls to external systems
+            val externalSSWithOutRel = mutableListOf<SoftwareSystem>()
+
+            allExternalSoftwareSystems.forEach { s ->
+                it.relationships
+                        .filter { r -> r.destination.id.equals(s.id) }
+                        .forEach { se ->
+                            externalSSWithOutRel.add(s)
+                        }
+            }
+
+            externalSSWithOutRel.forEach { linkedExternalSS ->
+
+                val relDig = diagrams.firstOrNull { v -> v.container.id == it.container.id }
+
+                if (relDig?.key?.isNotEmpty() == true)
+                {
+
+                    documents += Document(
+                            GetUrl(relDig.key, href),
+                            "Component views",
+                            "${softwareSystem.name} | Component views | ${it.container.name} | ${it.name} |  ${linkedExternalSS.name} | (OUTBOUND)",
+                            linkedExternalSS.name)
+                }
+
+            };                
+        }
     }
 
     return documents
@@ -106,4 +168,4 @@ fun softwareSystemComponents(softwareSystem: SoftwareSystem, viewModel: PageView
 private fun GetUrl(componentId : String, defaultUrl : String) : String
 {
     return "https://c4.lebara.com/master/svg/$componentId.svg"
-}    
+}
